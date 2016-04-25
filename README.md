0944440422328c47c34e32cc7959ec0e4da6b738
aereal <aereal@aereal.org>
2016-04-25 13:57:34 +0900

    minil new Plack-App-CronSchedule

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Plack::App::CronSchedule - It's new $module
+
+# SYNOPSIS
+
+    use Plack::App::CronSchedule;
+
+# DESCRIPTION
+
+Plack::App::CronSchedule is ...
+
+# LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+aereal &lt;aereal@aereal.org>
---
+++ lib/Plack/App/CronSchedule.pm
@@ -0,0 +1,39 @@
+package Plack::App::CronSchedule;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::App::CronSchedule - It's new $module
+
+=head1 SYNOPSIS
+
+    use Plack::App::CronSchedule;
+
+=head1 DESCRIPTION
+
+Plack::App::CronSchedule is ...
+
+=head1 LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+aereal E<lt>aereal@aereal.orgE<gt>
+
+=cut
+
